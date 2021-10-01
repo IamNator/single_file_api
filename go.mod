2869294702da5bbc5a10ef2dcd6d590fe98ffcbe
IamNator <natverior1@gmail.com>
2021-10-02 00:45:13 +0100

    ...

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ module github.com/IamNator/single_file_api
 go 1.16
 
 require (
-	gorm.io/driver/mysql v1.1.2
-	gorm.io/gorm v1.21.15
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,36 +1,86 @@
 package main
 
 import (
-  "encoding/json"
-  "gorm.io/driver/mysql"
-  "gorm.io/gorm"
+  "github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
   "log"
   "net/http"
   "os"
+  "time"
 )
 
 type Table struct {
+  ID        uint `gorm:"primary_key"`
   PowerStatus  bool `json:"powerStatus" schema:"powerStatus"`
-  gorm.Model
+  CreatedAt time.Time
+  UpdatedAt time.Time `json:"_"`
+  DeletedAt *time.Time `json:"_" sql:"index"`
 }
 
 func main(){
 
   //CLEARDB_DATABASE_URL
-  dsn := os.Getenv("CLEARDB_DATABASE_URL")
-  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  dsn := os.Getenv("DSN")
+  if dsn == "" {
+   //dsn = "b0c18710b783ac:7e825d69@eu-cdbr-west-01.cleardb.com/heroku_25194f2ea429b61?reconnect=true"
+   dsn = "b0c18710b783ac:7e825d69@tcp(eu-cdbr-west-01.cleardb.com)/heroku_25194f2ea429b61?parseTime=true"
+    }
+
+  db, err := gorm.Open( "mysql", dsn)
   if err != nil {
     log.Fatalf(err.Error())
   }
 
   er := db.AutoMigrate(&Table{})
-  if er != nil{
-    log.Fatalf(er.Error())
+  if er.Error != nil{
+    log.Fatalf(er.Error.Error())
   }
 
+  r := gin.Default()
 
-  
-  http.HandleFunc("/api/add", func (w http.ResponseWriter, r *http.Request){
+  r.GET("/api/hiii", func(ctx *gin.Context) {
+    ctx.JSON(200 ,  gin.H{"hi": "successful"})
+    return
+  })
+  r.GET("/api/add", add(db))
+  r.GET("/api/get",get(db))
+
+  port := ":"+os.Getenv("PORT")
+  if port == ":" {
+    port = ":3000"
+  }
+
+  log.Printf("server running on %s", port)
+  err  = r.Run(port)
+    if err != nil {
+      log.Fatalf(err.Error())
+    }
+
+    log.Println("server stoping")
+
+}
+
+func get(db *gorm.DB) func(ctx *gin.Context) {
+  return func (ctx *gin.Context){
+    w := ctx.Writer
+
+    t := make([]Table,1)
+    err := db.Model(Table{}).Find(&t).Error
+    if err != nil {
+      http.Error(w, err.Error(), 400)
+      return
+    }
+    w.WriteHeader(200)
+    w.Header().Set("content-type", "application/json")
+    ctx.JSON(200, t)
+  }
+}
+
+func add(db *gorm.DB) func(ctx *gin.Context) {
+  return func(ctx *gin.Context) {
+    w := ctx.Writer
+    r := ctx.Request
     var t Table
     s := r.FormValue("powerStatus")
     if s != "0" && s != "1" {
@@ -45,24 +95,15 @@ func main(){
       t.PowerStatus = true
     }
 
-    err = db.Model(Table{}).Save(&t).First(&t).Error
+    err := db.Model(Table{}).Create(&t).First(&t).Error
     if err != nil {
       http.Error(w, err.Error(), 400)
       return
     }
+
+    w.WriteHeader(201)
     w.Header().Set("content-type", "application/json")
-    json.NewEncoder(w).Encode(t)
-  })
-  
-   http.HandleFunc("/api/get", func (w http.ResponseWriter, r *http.Request){
-     var t []Table
-     err = db.Model(Table{}).Find(&t).Error
-     if err != nil {
-       http.Error(w, err.Error(), 400)
-       return
-     }
-     w.Header().Set("content-type", "application/json")
-     json.NewEncoder(w).Encode(t)
-  })
-  http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+    ctx.JSON(200, t)
+  }
+
 }
