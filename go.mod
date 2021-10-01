5c2a1da0ae61c9a9a97ef4d9dea9b7d86689767e
IamNator <natverior1@gmail.com>
2021-10-01 22:25:59 +0100

    basic setup

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/IamNator/single_file_api
 go 1.16
 
 require (
+	github.com/gorilla/schema v1.2.0
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.15
 )
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-  "net/http"
-  "os"
-  "log"
+  "encoding/json"
+  "github.com/gorilla/schema"
   "gorm.io/driver/mysql"
   "gorm.io/gorm"
+  "log"
+  "net/http"
+  "os"
 )
+
+type Table struct {
+  Hello  string `json:"hello" schema:"hello"`
+  Name    string `json:"name" schema:"name"`
+  gorm.Model
+}
 
 func main(){
 
@@ -14,15 +22,43 @@ func main(){
   dsn := os.Getenv("CLEARDB_DATABASE_URL")
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
   if err != nil {
-    log.Println(err.Error())
+    log.Fatalf(err.Error())
   }
+
+  er := db.AutoMigrate(&Table{})
+  if er != nil{
+    log.Fatalf(er.Error())
+  }
+
+
   
   http.HandleFunc("/api/add", func (w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("hello add..."))
+    var t Table
+    err := schema.NewDecoder().Decode(&t, r.Form)
+    if err != nil {
+      http.Error(w, err.Error(), 400)
+      return
+    }
+
+    err = db.Model(Table{}).Where("hello = ?", t.Hello).Updates(&t).FirstOrCreate(&t).First(&t).Error
+    if err != nil {
+      http.Error(w, err.Error(), 400)
+      return
+    }
+    w.Header().Set("content-type", "application/json")
+    json.NewEncoder(w).Encode(t)
   })
   
    http.HandleFunc("/api/get", func (w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("hello get..."))
+     var t []Table
+
+     err = db.Model(Table{}).Where("hello = ?", r.FormValue("hello")).Find(&t).Error
+     if err != nil {
+       http.Error(w, err.Error(), 400)
+       return
+     }
+     w.Header().Set("content-type", "application/json")
+     json.NewEncoder(w).Encode(t)
   })
   http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
