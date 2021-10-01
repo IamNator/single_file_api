798a84f4b333082975335c0052ed95637d516e5b
Nator Verinumbe <natverior1@gmail.com>
2021-10-01 21:42:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
   "net/http"
   "os"
+  "log"
+  "gorm.io/driver/mysql"
+  "gorm.io/gorm"
 )
 
 func main(){
 
   //CLEARDB_DATABASE_URL
+  dsn := os.Getenv("CLEARDB_DATABASE_URL")
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  if err != nil {
+    log.Println(err.Error())
+  }
+  
   http.HandleFunc("/api/add", func (w http.ResponseWriter, r *http.Request){
     w.Write([]byte("hello add..."))
   })
