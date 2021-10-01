183c8a51b610506d6f439ef0eb2e843af134d391
Nator Verinumbe <natverior1@gmail.com>
2021-10-01 21:29:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main(){
 
   //CLEARDB_DATABASE_URL
   http.HandlerFunc("/api/add", func (){
-    w.Write([]byte("hello ..."))
+    w.Write([]byte("hello add..."))
   })
   
    http.HandlerFunc("/api/get", func (){
-    w.Write([]byte("hello ..."))
+    w.Write([]byte("hello get..."))
   })
   http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
